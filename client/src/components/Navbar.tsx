ff1b8f62814ac41d0a8f164358a4e73bb061ff32
Vincent CHING <chingvincentdu20@yahoo.fr>
2024-12-03 15:04:59 +0100

    feat : Navbar finished mediascreen and mobile

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import type React from "react";
+import logo from "../assets/images/round_logo.png";
+import "./index.css"; // Assurez-vous d'importer le fichier CSS
+
+const Navbar: React.FC = () => {
+	return (
+		<nav>
+			<img src={logo} alt="Logo" />
+			<h1>Coup de main Local</h1>
+			<ul>
+				<li>
+					<button type="button">Annuaire</button>
+				</li>
+				<li>
+					<button type="button">Annonces</button>
+				</li>
+			</ul>
+		</nav>
+	);
+};
+
+export default Navbar;
