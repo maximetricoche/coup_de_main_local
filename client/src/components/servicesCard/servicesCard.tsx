209da362b8bc393caa530f525c2d0f85c8fe39d9
Marvin Calmo <calmomarvin93@gmail.com>
2024-12-03 16:26:57 +0100

    [feat] : ServicesCard

---
+++ client/src/components/servicesCard/servicesCard.tsx
@@ -0,0 +1,28 @@
+import "./ServicesCard.css";
+
+function ServicesCard() {
+  return (
+    <figure className="ServicesDisplayCard">
+      <div className="ColumnServices">
+        <div className="ServicesImg">
+          <img src="" alt="Service" />
+        </div>
+        <div className="ServicesCategory">
+          <div className="ServicesCategoryIcon1">
+            <p>Categorie</p>
+          </div>
+          <div className="ServicesCategoryIcon2">
+            <p>Echange</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="ServicesDetails">
+        <h1>Titre</h1>
+        <p>Description</p>
+      </div>
+    </figure>
+  );
+}
+
+export default ServicesCard;
