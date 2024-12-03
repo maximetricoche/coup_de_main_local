2ada278b8939afa917fc8a94ddeff5a53acf1670
Vincent CHING <chingvincentdu20@yahoo.fr>
2024-12-03 15:27:12 +0100

    Feat : fix bug

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,13 +3,13 @@ import Footer from "./components/Footer";
 import NavBar from "./components/Navbar";
 
 const App = () => {
-	return (
-		<div>
-			<NavBar />
-			<Outlet />
-			<Footer />
-		</div>
-	);
+  return (
+    <div>
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
