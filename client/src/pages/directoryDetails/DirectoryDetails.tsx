183425e33f9376fa8697933ca6c27d6a82c487f3
Amandine GRARD-LAURENT <a.grard.laurent08@gmail.com>
2024-12-03 21:47:33 +0100

    fix API data

--- client/src/pages/directoryDetails/DirectoryDetails.tsx
+++ client/src/pages/directoryDetails/DirectoryDetails.tsx
@@ -1,9 +1,13 @@
+import { useLoaderData } from "react-router-dom";
 import "./DirectoryDetails.css";
 
 const DirectoryDetails = () => {
+  const data = useLoaderData();
+  console.info(data);
   return (
     <div className="directory-details">
-      <h2>Détails de l'Annuaire</h2>
+      <h2>Nom de l'entreprise :</h2>
+      <h3>Adresse : </h3>
     </div>
   );
 };
