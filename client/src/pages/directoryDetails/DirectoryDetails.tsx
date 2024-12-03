2e68f15edcea2a5a70102a0805a4a204f9093146
Amandine GRARD-LAURENT <a.grard.laurent08@gmail.com>
2024-12-04 00:56:07 +0100

    feat: services details et directory details pages

--- client/src/pages/directoryDetails/DirectoryDetails.tsx
+++ client/src/pages/directoryDetails/DirectoryDetails.tsx
@@ -1,14 +1,34 @@
 import { useLoaderData } from "react-router-dom";
 import "./DirectoryDetails.css";
 
+type directoryType = {
+  id: number;
+  nom: string;
+  adresse: string;
+  tel: string;
+  horaires_ouverture: string;
+  categorie: string;
+  image: string;
+};
+
 const DirectoryDetails = () => {
-  const data = useLoaderData();
+  const data: directoryType = useLoaderData() as directoryType;
   console.info(data);
   return (
-    <div className="directory-details">
-      <h2>Nom de l'entreprise :</h2>
-      <h3>Adresse : </h3>
-    </div>
+    <main className="directory-details">
+      <section>
+        <img src={data.image} alt={data.nom} className="image" />
+      </section>
+      <section className="informations">
+        <div className="categorie">
+          <p>{data.categorie}</p>
+        </div>
+        <h2>{data.nom}</h2>
+        <h3>Adresse : {data.adresse} </h3>
+        <h3>Coordonnées téléphonique : {data.tel}</h3>
+        <h3>Horaires d'ouverture : {data.horaires_ouverture}</h3>
+      </section>
+    </main>
   );
 };
 
--- client/src/pages/servicesDetails/ServicesDetails.tsx
+++ client/src/pages/servicesDetails/ServicesDetails.tsx
@@ -1,10 +1,36 @@
+import { useLoaderData } from "react-router-dom";
 import "./ServicesDetails.css";
 
+type serviceType = {
+  id: number;
+  nom: string;
+  prenom: string;
+  adresse: string;
+  description: string;
+  categorie: string;
+  type: string;
+  échange: string;
+  image: string;
+};
+
 const ServicesDetails = () => {
+  const data: serviceType = useLoaderData() as serviceType;
   return (
-    <div className="services-details">
-      <h2>Détails du Service</h2>
-    </div>
+    <main className="wrapper">
+      <section>
+        <img src={data.image} alt={data.nom} className="image" />
+      </section>
+      <section className="informations">
+        <h2>
+          {data.prenom} {data.nom} : {data.categorie}
+        </h2>
+        <h3>Catégorie : {data.type}</h3>
+        <p>{data.description}</p>
+        <p>Type d'échange : {data.échange}</p>
+        <h4>Adresse : {data.adresse} </h4>
+        <button type="button">Intéréssé ? Contactez-moi</button>
+      </section>
+    </main>
   );
 };
 
